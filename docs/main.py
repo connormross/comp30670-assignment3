3e13f1082f5df662976184ec1c96ec4a4b2072d4
connorross <connorross@192.168.192.46>
2017-03-06 13:55:52 +0000

    Commit after completing the main() function and testing it with input using the command line

--- docs/main.py
+++ docs/main.py
@@ -3,13 +3,10 @@ Created on 1 Mar 2017
 
 @author: connorross
 '''
-import sys 
+
 import argparse
-import numpy
-from et_xmlfile.tests.common_imports import read_file
 import re 
 from builtins import len 
-from argcomplete.compat import str
 import urllib.request
 
 
@@ -25,7 +22,39 @@ def main():
     req = urllib.request.urlopen(argmts)
 
     buffer = req.read().decode('utf-8')
-    print(buffer)
+    
+    lines = buffer.split('\n')
+    
+    r = re.compile("([0-9]+)")
+    
+    lights = set()
+    
+    for line in lines[1:-1]:
+        print(line)
+        x1, y1, x2, y2 = [int(x) for x in r.findall(line)]
+        if line.startswith('switch'):
+            for x in range(x1, x2+1):
+                for y in range(y1, y2+1):
+                    if(x,y) in lights:
+                        lights.remove((x,y))
+                    else:
+                        lights.add((x,y))
+                            
+        elif line.startswith('turn off'):
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    if (x,y) in lights:
+                        lights.remove((x,y))
+                            
+        elif line.startswith('turn on'):
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    lights.add((x,y))  
+                        
+        
+        
+        
+    print (len(lights))
 
     
     
